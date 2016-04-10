1b232e526702d8cb13d7fd7cfd4755f9a5b3dace
Bruno Ricardo <brunoricardojava@gmail.com>
2016-04-10 11:38:27 -0300

    Codigo pego da internet. Falta modificar

---
+++ AVL/src/ArvoreAVL/No.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArvoreAVL;
+
+/**
+ *
+ * @author bruno
+ */
+public class No {
+    
+    private No esquerda;
+	private No direita;
+	private No pai;
+	private int chave;
+	private int balanceamento;
+
+	public No(int k) {
+		setEsquerda(setDireita(setPai(null)));
+		setBalanceamento(0);
+		setChave(k);
+	}
+
+	public String toString() {
+		return Integer.toString(getChave());
+	}
+
+	public int getChave() {
+		return chave;
+	}
+
+	public void setChave(int chave) {
+		this.chave = chave;
+	}
+
+	public int getBalanceamento() {
+		return balanceamento;
+	}
+
+	public void setBalanceamento(int balanceamento) {
+		this.balanceamento = balanceamento;
+	}
+
+	public No getPai() {
+		return pai;
+	}
+
+	public No setPai(No pai) {
+		this.pai = pai;
+		return pai;
+	}
+
+	public No getDireita() {
+		return direita;
+	}
+
+	public No setDireita(No direita) {
+		this.direita = direita;
+		return direita;
+	}
+
+	public No getEsquerda() {
+		return esquerda;
+	}
+
+	public void setEsquerda(No esquerda) {
+		this.esquerda = esquerda;
+	}
+    
+}
